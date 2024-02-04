0dee4c347c87e0335f49a54a02d2534149f53a3f
JoshuaDuquette2 <joshuaduquette2@g.ucla.edu>
2024-02-04 14:19:52 -0800

    fix

--- webserver/detector_webserver/driver/settings.py
+++ webserver/detector_webserver/driver/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gentelella.urls'
+ROOT_URLCONF = 'driver.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gentelella.wsgi.application'
+WSGI_APPLICATION = 'driver.wsgi.application'
 
 
 # Database
